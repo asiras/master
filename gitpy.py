0aeba80c8a24735935d70f25ed4003a6a233e208
asiras <55871222+asiras@users.noreply.github.com>
2021-02-19 12:56:30 +0530

    Add files via upload

---
+++ gitpy.py
@@ -0,0 +1,58 @@
+from git import Repo
+#Repo.clone_from("https://github.com/saamaresearch/pfizer-3rdHackathon-smpc-pil.git", "D://test_git//")
+import os
+COMMIT_MESSAGE = 'comment from python script'
+repo_path="D://test_git//"
+COMMITS_TO_PRINT = 5
+
+def print_commit_data(commit):
+    print('-----')
+    print(str(commit.hexsha))
+    print("\"{}\" by {} ({})".format(commit.summary, commit.author.name, commit.author.email))
+    print(str(commit.authored_datetime))
+    print(str("count: {} and size: {}".format(commit.count(), commit.size)))
+
+
+def print_repository_info(repo):
+    print('Repository description: {}'.format(repo.description))
+    print('Repository active branch is {}'.format(repo.active_branch))
+
+    for remote in repo.remotes:
+        print('Remote named "{}" with URL "{}"'.format(remote, remote.url))
+
+    print('Last commit for repository is {}.'.format(str(repo.head.commit.hexsha)))
+
+def git_push():
+    try:
+        repo = Repo(repo_path)
+        repo.git.add(update=True)
+        repo.index.commit(COMMIT_MESSAGE)
+        origin = repo.remote(name='main')
+        origin.push()
+    except:
+        print('Some error occured while pushing the code')    
+
+
+
+
+if __name__=='__main__':
+    #repo_path = os.getenv('REPO_PATH')
+    
+    # Repo object used to interact with Git repositories
+    repo = Repo(repo_path)
+
+    # check that the repository loaded correctly
+    if not repo.bare:
+        print('Repo at {} successfully loaded.'.format(repo_path))
+        print_repository_info(repo)
+
+        # create list of commits then print some of them to stdout
+        commits = list(repo.iter_commits('main'))[:COMMITS_TO_PRINT]
+        for commit in commits:
+            print_commit_data(commit)
+            pass
+
+    else:
+        print('Could not load repository at {} :'.format(repo_path))
+
+    git_push()
