443c8ece4471903b610a219054e0a5a6a37aff1a
Akshay Siras <akshay.Siras@saama.com>
2021-02-22 13:18:49 +0550

    comment from python script

--- auth.py
+++ auth.py
@@ -1,10 +1,22 @@
+# import os
+# from pprint import pprint
+# import requests
+# token ='5ae7502121950f43cb65b98aa4d26f5394c87e62'
+
+# headers = {'Authorization': 'token ' + token}
+
+# login = requests.get('https://api.github.com/user', headers=headers)
+# pprint(login.json())
+
+
+
 from github import Github
 import os
 from pprint import pprint
 import urllib3
 token ='5ae7502121950f43cb65b98aa4d26f5394c87e62'
-# g = Github(token)
-# repo = g.get_repo("asiras/master")
-# print(repo.pull())
-# issues = repo.get_issues(state="open")
-# pprint(issues.get_page(0))
+g = Github(token)
+for repo in g.get_user().get_repos():
+    print(repo.name)
+
+g.get_repo('master')
