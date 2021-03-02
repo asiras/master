5011078d7e822235c71f6bab7eb2ee6743c93ab6
Akshay Siras <akshay.Siras@saama.com>
2021-03-02 17:38:27 +0550

    update

--- gitpy.py
+++ gitpy.py
@@ -135,6 +135,6 @@ if __name__=='__main__':
     # else:
     #     print('Could not load repository at {} :'.format(repo_path))
     
-    #create_new_branch(repo)#create new branch
-    git_push()
+    create_new_branch(repo)#create new branch
+    #git_push()
     
